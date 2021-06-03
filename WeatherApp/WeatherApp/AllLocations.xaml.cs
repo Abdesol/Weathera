a941b746ddcf28dd3b83d7299a543a066bc8dee1
Abdesol <abdesoltak@gmail.com>
2021-06-03 20:00:05 +0300

    added picker

---
+++ WeatherApp/WeatherApp/AllLocations.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllLocations : ContentPage
+    {
+        public AllLocations()
+        {
+            InitializeComponent();
+        }
+
+        public async void Back_Clicked(object sender, EventArgs args)
+        {
+            await back_btn.ScaleTo(0.75, 100);
+            await back_btn.ScaleTo(1, 100);
+
+            App.Current.MainPage = new MainPage();
+        }
+    }
+}
--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -13,13 +13,55 @@ namespace WeatherApp
         public MainPage()
         {   
             InitializeComponent();
+
+            locations.Title = "Locations"; locations.TextColor = Color.White;
+
+            List<string> all_locations = new List<string>() 
+            {"Addis Ababa, ET",
+            "Seattle, WA",
+            "LosAngles, CA",
+            "Diredwa, ET",
+            "Nairobi, KE",
+            "Mekele, ET",
+            "Mumbai, IN",
+            "Tokyo, JP"
+            };
+
+            foreach(string loc in all_locations)
+            {
+                locations.Items.Add(loc);
+            }
+
+            locations.SelectedIndexChanged += (sender, args) =>
+            {
+                if(locations.SelectedIndex == -1)
+                {
+                    current_location.Text = all_locations[0];
+                }
+                else
+                {
+                    current_location.Text = locations.Items[locations.SelectedIndex];
+                }
+            };
         }
         
-        public async void MenuBtn_Clicked(object sender, EventArgs args)
+        public async void RefreshBtn_Clicked(object sender, EventArgs args)
         {
-            await menu_btn.ScaleTo(0.75, 100);
-            await menu_btn.ScaleTo(1, 100);
+            await refresh_btn.ScaleTo(0.75, 60);
+            await refresh_btn.ScaleTo(1, 60);
+            for (int i=1; i<=6; i++)
+            {
+                if (refresh_btn.Rotation >= 360f) refresh_btn.Rotation = 0;
+                await refresh_btn.RotateTo(i * (360 / 6), 1000, Easing.Linear);
+            }
         }
 
+        public async void ChangeLoc_Clicked(object sender, EventArgs args)
+        {
+            await location_btn.ScaleTo(0.75, 100);
+            await location_btn.ScaleTo(1, 100);
+
+            App.Current.MainPage = new AllLocations();
+        }
     }
 }
